24eb354376c200c9e9e18220dc7e228f549adc0f
kb995 <t.kuboshima995@gmail.com>
2020-07-14 05:35:55 +0900

    lesson4

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,14 +2,28 @@
 
 namespace App\Http\Controllers;
 use App\Article;
+use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    // 一覧表示
     public function index()
     {
         $articles = Article::all()->sortByDesc('created_at');
         return view('articles.index', ['articles' => $articles]);
+    }
+    // 投稿画面表示
+    public function create()
+    {
+        return view('articles.create');    
+    }
+
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all());
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
     }
 }
