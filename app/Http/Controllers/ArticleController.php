f8d24dc1ab1f0455133b89344ebb56c5fb3eaf16
kb995 <t.kuboshima995@gmail.com>
2020-07-15 03:23:12 +0900

    lesson6

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -19,11 +19,38 @@ class ArticleController extends Controller
         return view('articles.create');    
     }
 
+    // 投稿保存
     public function store(ArticleRequest $request, Article $article)
     {
         $article->fill($request->all());
         $article->user_id = $request->user()->id;
         $article->save();
         return redirect()->route('articles.index');
+    }
+
+    // 編集画面
+    public function edit(Article $article)
+    {
+        return view('articles.edit', ['article' => $article]);
+    }
+
+    // 編集処理
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all())->save();
+        return redirect()->route('articles.index');
+    }
+
+    // 削除処理
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
+    }
+
+    // 詳細表示
+    public function show(Article $article)
+    {
+        return view('articles.show', ['article' => $article]);
     }
 }
